aa42ee82f6d7ae3695a7d179639926fafa95e768
Passion <fabian@easy-emu.de>
2010-05-13 18:23:49 +0200

    Switch back to 4.0.0. grrrrrrrrrrrrrrrr-.-

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -284,9 +284,8 @@ void Object::BuildMovementUpdate(ByteBuffer * data, uint16 updateFlags) const
                         }
                     }
                 }
-                if(((Unit*)this)->GetVehicleGUID())
-                    moveFlags2 |= (MOVEFLAG_ONTRANSPORT | MOVEFLAG_ROOT);
-
+				if(((Unit*)this)->GetVehicleGUID())
+					moveFlags2 |= (MOVEFLAG_ONTRANSPORT | MOVEFLAG_ROOT);
             }
             break;
             case TYPEID_PLAYER:
@@ -301,8 +300,9 @@ void Object::BuildMovementUpdate(ByteBuffer * data, uint16 updateFlags) const
                 // remove unknown, unused etc flags for now
                 player->m_movementInfo.RemoveMovementFlag(MOVEFLAG_SPLINE_ENABLED);
 
-                if(((Unit*)this)->GetVehicleGUID())
-                    moveFlags2 |= (MOVEFLAG_ONTRANSPORT | MOVEFLAG_ROOT);
+				if(((Unit*)this)->GetVehicleGUID())
+					moveFlags2 |= (MOVEFLAG_ONTRANSPORT | MOVEFLAG_ROOT);
+
                 if(player->isInFlight())
                 {
                     ASSERT(player->GetMotionMaster()->GetCurrentMovementGeneratorType() == FLIGHT_MOTION_TYPE);
