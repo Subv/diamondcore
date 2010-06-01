e766dcb26fdd5e927c622c224472437d85c84067
Fabian <fabian@easy-emu.de>
2010-06-01 20:12:12 +0200

    [Alpha Rev 109] Implement Treshold in Masterloot. Fixed triggering of spell 55095 by 45524. Fix 16164 proc conditions Additional check for HandleAutoBankItemOpcode. Typo in BattleGroundQueue::AddGroup arg name Fixed selection BG map difficulty. Restore raid functionality Use special function for honored/xp kill check Fixed typo in recently added group enums. Update talent 16180 and ranks work. Add explicit AP damage bonus for spells 55095, 55095. Update max cooldown length req. for spell casting at arena. Remove target guid in spell with TARGET_FLAG_DEST_LOCATION For non-player at "offline" delete hostile references. Group out of range member must based at client existances. Leave at AFK applied only to battlegrounds (not arena) Implement item 43214 work. Send SMSG_CORPSE_NOT_IN_INSTANCE if player in ghost form tries to enter an instance without player's corpse. Talent 51459 and ranks affect only melee auto-attacks. Restore work talent 12311 and ranks. Add correct spell chain data for 61024 and 61316. Next step in fix SMSG_GROUP_LIST structure.
    Now assistant/main tank/main assistant show properly
    for viewer character also.
    
    Fixed MSG_PARTY_ASSIGNMENT processing.
    Limit spell 50988, 59870 targets amounts to expected.
    Fixed a bug/typo that prevent to summon a special flying mount.

---
+++ sql/Updates/Alpha_109_01_world_spell_proc_event.sql
@@ -0,0 +1,3 @@
+delete from `spell_proc_event` where entry = 16164;
+
+insert into `spell_proc_event` values (16164, 0x0000001C, 11, 0x00000000, 0x00000000, 0x00000000, 0x00010000, 0x00000002, 0.000000, 0.000000,  0);
---
+++ sql/Updates/Alpha_109_05_world_spell_chain.sql
@@ -0,0 +1,13 @@
+
+
+DELETE FROM  `spell_chain` WHERE `spell_id` IN (61024,61316);
+
+INSERT INTO `spell_chain` (`spell_id`, `prev_spell`, `first_spell`, `rank`, `req_spell`) VALUES
+
+/*Dalaran Intellect*/
+
+(61024,0,61024,1,27126),
+
+/*Dalaran Brilliance*/
+
+(61316,0,61316,1,27127);
