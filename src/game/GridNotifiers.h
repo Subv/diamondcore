81661e9bef0737dab29e1ae029e072afc69faf55
Fabian <fabian@easy-emu.de>
2010-06-07 12:32:14 +0200

    [Alpha Rev 128] Remove farsight auras at viewpoint visibility changes Restore dynamic object visibility checks Thanks SilverIce.

--- src/game/GridNotifiers.h
+++ src/game/GridNotifiers.h
@@ -97,7 +97,7 @@ namespace Diamond
         MessageDelivererExcept(WorldObject const* obj, WorldPacket *msg, Player const* skipped)
             : i_phaseMask(obj->GetPhaseMask()), i_message(msg), i_skipped_receiver(skipped) {}
         
-        void Visit(PlayerMapType &m);
+        void Visit(CameraMapType &m);
         template<class SKIP> void Visit(GridRefManager<SKIP> &) {}
     };
 
